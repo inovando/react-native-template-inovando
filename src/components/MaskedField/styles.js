2c8ee965d5626a2964d5b19c38a3bd267cda51e8
Patrick Santos <patrickspteixeira@gmail.com>
2020-06-22 12:07:20 -0300

    update input styles

--- src/components/MaskedField/styles.js
+++ src/components/MaskedField/styles.js
@@ -1,12 +1,7 @@
 import { TextInputMask } from 'react-native-masked-text';
 import styled from 'styled-components/native';
+import { inputStyles } from 'components/TextField/styles';
 
 export const Input = styled(TextInputMask)`
-  border: 1px solid #808080;
-  height: 45px;
-  border-radius: 4px;
-  color: #000;
-  padding: 0 10px;
-  background-color: #fff;
-  opacity: ${(props) => (props.editable ? 1 : 0.3)};
+  ${inputStyles}
 `;
--- src/components/TextField/styles.js
+++ src/components/TextField/styles.js
@@ -1,11 +1,11 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
 export const Container = styled.View`
   align-self: stretch;
   margin-bottom: 15px;
 `;
 
-export const Input = styled.TextInput`
+export const inputStyles = css`
   border: 1px solid #808080;
   height: 45px;
   border-radius: 4px;
@@ -13,6 +13,10 @@ export const Input = styled.TextInput`
   padding: 0 10px;
   background-color: #fff;
   opacity: ${(props) => (props.editable ? 1 : 0.3)};
+`;
+
+export const Input = styled.TextInput`
+  ${inputStyles}
 `;
 
 export const ErrorText = styled.Text`
